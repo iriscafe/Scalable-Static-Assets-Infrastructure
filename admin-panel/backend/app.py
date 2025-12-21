39bc86c44b993cd55a6734e113c8d7c1bf55fd79
iris.cafe <irislisboa.c@gmail.com>
2025-12-21 01:17:18 -0300

    fix formataçao url do cloudfront no front

--- admin-panel/backend/app.py
+++ admin-panel/backend/app.py
@@ -10,6 +10,8 @@ FRONTEND_DIR = os.path.join(BASE_DIR, "frontend")
 @app.route("/")
 def index():
     cloudfront_url = os.getenv("CLOUDFRONT_URL", "https://CDN_NOT_CONFIGURED")
+    if cloudfront_url and not cloudfront_url.startswith("http"):
+        cloudfront_url = f"https://{cloudfront_url}"
     
     try:
         with open(os.path.join(FRONTEND_DIR, "index.html"), "r") as f:
